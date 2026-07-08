6629fa2a82a8246e730ff54d5bb69adfd6e47bac
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-07-08 13:47:45 +0200

    use list of VMs in conf

--- bin/conf.ml
+++ bin/conf.ml
@@ -1,13 +1,27 @@
-let my_vm : Vm.vm =
-  {
-    name = "my_vm";
-    memory = 4096;
-    cores = 4;
-    disks = [ { ty = Vm.Qcow2; path = "disk.qcow2" } ];
-    redirections =
-      [
-        { ty = Vm.Tcp; port_host = 8022; port_vm = 22 };
-        { ty = Vm.Tcp; port_host = 8443; port_vm = 443 };
-        { ty = Vm.Tcp; port_host = 8080; port_vm = 80 };
-      ];
-  }
+let vms : Vm.vm list =
+  [
+    {
+      name = "vm1";
+      memory = 4096;
+      cores = 4;
+      disks = [ { ty = Vm.Qcow2; path = "disk_vm1.qcow2" } ];
+      redirections =
+        [
+          { ty = Vm.Tcp; port_host = 8022; port_vm = 22 };
+          { ty = Vm.Tcp; port_host = 8443; port_vm = 443 };
+          { ty = Vm.Tcp; port_host = 8080; port_vm = 80 };
+        ];
+    };
+    {
+      name = "vm2";
+      memory = 4096;
+      cores = 4;
+      disks = [ { ty = Vm.Qcow2; path = "disk_vm2.qcow2" } ];
+      redirections =
+        [
+          { ty = Vm.Tcp; port_host = 8022; port_vm = 22 };
+          { ty = Vm.Tcp; port_host = 8443; port_vm = 443 };
+          { ty = Vm.Tcp; port_host = 8080; port_vm = 80 };
+        ];
+    };
+  ]
--- bin/xcp_launcher.ml
+++ bin/xcp_launcher.ml
@@ -1,8 +1,10 @@
 let qemu_system = "qemu-system-x86_64"
 
 let main out proc_mgr =
+  (* TODO: process all VMs, while testing only consider the first one *)
+  let vm = List.hd Conf.vms in
   Eio.Process.run proc_mgr [ qemu_system; "--version" ];
-  let cmd = String.concat " " (qemu_system :: Vm.vm_to_args Conf.my_vm) in
+  let cmd = String.concat " " (qemu_system :: Vm.vm_to_args vm) in
   Eio.Flow.copy_string (cmd ^ "\n") out
 
 let () =
