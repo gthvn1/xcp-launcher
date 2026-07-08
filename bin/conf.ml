dbaf7e05713eb011d36f3f675db26990087612bc
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-07-08 14:02:52 +0200

    use base_dir to get disks and uefi vars

--- bin/conf.ml
+++ bin/conf.ml
@@ -1,10 +1,14 @@
 let vms : Vm.vm list =
   [
     {
+      (* base_dir is relative to HOME, if HOME doesn't exist when VM is
+         started the program will fail. *)
+      base_dir = "qemu-env/xcpng/8.3";
       name = "vm1";
       memory = 4096;
       cores = 4;
-      disks = [ { ty = Vm.Qcow2; path = "disk_vm1.qcow2" } ];
+      uefi_vars = "my_vars.fd";
+      disks = [ { ty = Vm.Qcow2; path = "disk.qcow2" } ];
       redirections =
         [
           { ty = Vm.Tcp; port_host = 8022; port_vm = 22 };
@@ -13,15 +17,12 @@ let vms : Vm.vm list =
         ];
     };
     {
+      base_dir = "qemu-env/xcpng/8.3";
       name = "vm2";
       memory = 4096;
       cores = 4;
+      uefi_vars = "uefi_vars_vm2.fd";
       disks = [ { ty = Vm.Qcow2; path = "disk_vm2.qcow2" } ];
-      redirections =
-        [
-          { ty = Vm.Tcp; port_host = 8022; port_vm = 22 };
-          { ty = Vm.Tcp; port_host = 8443; port_vm = 443 };
-          { ty = Vm.Tcp; port_host = 8080; port_vm = 80 };
-        ];
+      redirections = [];
     };
   ]
--- bin/vm.ml
+++ bin/vm.ml
@@ -4,9 +4,11 @@ type redir_ty = Udp | Tcp
 type redirection = { ty : redir_ty; port_host : int; port_vm : int }
 
 type vm = {
+  base_dir : string; (* this is where we will look for disks for example *)
   name : string;
   memory : int;
   cores : int;
+  uefi_vars : string;
   disks : disk list;
   redirections : redirection list;
 }
@@ -14,14 +16,20 @@ type vm = {
 let disk_ty_to_string = function Qcow2 -> "qcow2" | Raw -> "raw"
 let redir_ty_to_string = function Udp -> "udp" | Tcp -> "tcp"
 
-let disk_to_args (disk_id : int) (disk : disk) : string list =
-  [
-    "-drive";
-    "file=" ^ disk.path ^ ",if=none,format=" ^ disk_ty_to_string disk.ty
-    ^ ",id=hd" ^ string_of_int disk_id;
-    "-device";
-    "scsi-hd,drive=hd" ^ string_of_int disk_id;
-  ]
+let disks_to_args (disks : disk list) (vm_dir : string) : string list =
+  List.mapi
+    (fun id disk ->
+      let disk_id = string_of_int id in
+      let disk_path = vm_dir ^ "/" ^ disk.path in
+      [
+        "-drive";
+        "file=" ^ disk_path ^ ",if=none,format=" ^ disk_ty_to_string disk.ty
+        ^ ",id=hd" ^ disk_id;
+        "-device";
+        "scsi-hd,drive=hd" ^ disk_id;
+      ])
+    disks
+  |> List.concat
 
 let redirection_to_hostfwd redirection : string =
   Printf.sprintf "hostfwd=%s::%d-:%d"
@@ -33,7 +41,12 @@ let redirections_to_args redirections : string list =
   [ "-netdev"; String.concat "," ("user,id=net0" :: r) ]
 
 let vm_to_args (vm : vm) : string list =
-  let disks_args = List.mapi disk_to_args vm.disks |> List.concat in
+  let vm_dir =
+    match Sys.getenv_opt "HOME" with
+    | Some d -> d ^ "/" ^ vm.base_dir
+    | None -> failwith "HOME doesn't exist"
+  in
+  (* TODO: probably pass the OVMF path as a VM field *)
   [
     "-enable-kvm";
     "-m";
@@ -47,7 +60,7 @@ let vm_to_args (vm : vm) : string list =
     "-drive";
     "if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.fd";
     "-drive";
-    "if=pflash,format=raw,file=./my_vars.fd";
+    "if=pflash,format=raw,file=" ^ vm_dir ^ "/" ^ vm.uefi_vars;
     "-device";
     "virtio-scsi-pci,id=scsi";
     "-device";
@@ -55,5 +68,5 @@ let vm_to_args (vm : vm) : string list =
     "-boot";
     "c";
   ]
-  @ disks_args
+  @ disks_to_args vm.disks vm_dir
   @ redirections_to_args vm.redirections
